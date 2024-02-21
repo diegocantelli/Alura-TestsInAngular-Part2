b0d16cab7582161a51919697cc22ede13e684b3e
diegocantelli <diegocantelli19@gmail.com>
2024-02-21 17:40:22 -0300

    Criando a estrutura inicial do componente photo-frame

---
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-frame',
+  styleUrls: ['./photo-frame.component.scss'],
+  templateUrl: './photo-frame.component.html'
+})
+export class PhotoFrameComponent{}
