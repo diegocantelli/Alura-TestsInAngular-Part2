341f5dc9de5127d7e597205833b521d61238a5eb
diegocantelli <diegocantelli19@gmail.com>
2024-02-23 14:36:56 -0300

    Criando uma diretiva que escuta eventos do componente

---
+++ src/app/shared/directives/action/action.direction.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ActionDirection } from './action.direction';
+
+@NgModule({
+  declarations: [ActionDirection],
+  imports: [CommonModule],
+  exports: [ActionDirection]
+})
+export class ActionDirectiveModule{}
