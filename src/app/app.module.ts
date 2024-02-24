f7aef02886569286f0a80b99420d724ecb49f5ac
diegocantelli <diegocantelli19@gmail.com>
2024-02-24 18:52:14 -0300

    Carregando as fotos com base na api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
 import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
+import { PhotoBoardModule } from './shared/components/photo-board/photo-board.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.mo
     AppRoutingModule,
     LikeWidgetModule,
     PhotoFrameModule,
+    PhotoBoardModule,
     HttpClientModule
   ],
   providers: [],
