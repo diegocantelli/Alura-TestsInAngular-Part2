02867f2d18b31f65270dcbe049c56626e87d63b7
diegocantelli <diegocantelli19@gmail.com>
2024-02-25 09:32:31 -0300

    Criando o componente PhotoList

--- src/app/shared/components/photo-board/photo-board.module.ts
+++ src/app/shared/components/photo-board/photo-board.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { PhotoFrameModule } from '../photo-frame/photo-frame.module';
 import { PhotoBoardComponent } from './photo-board.component';
 import { PhotoBoardService } from './services/photo-board.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [CommonModule, PhotoFrameModule],
+  imports: [CommonModule, PhotoFrameModule, HttpClientModule],
   exports: [PhotoBoardComponent],
   declarations: [PhotoBoardComponent],
   providers: [PhotoBoardService]
