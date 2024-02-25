1d4144d7b06979e20cf7a349498c2c2b5b8dbf26
diegocantelli <diegocantelli19@gmail.com>
2024-02-25 12:21:18 -0300

    Criando uma logica de transformacao dos dados retornados pelo PhotoListService

--- src/app/shared/components/photo-board/services/photo-board.service.ts
+++ src/app/shared/components/photo-board/services/photo-board.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Photo } from '../interface/photo';
-import { delay } from 'rxjs/operators';
+import { delay, map } from 'rxjs/operators';
 
 @Injectable()
 export class PhotoBoardService{
@@ -11,6 +11,11 @@ export class PhotoBoardService{
 
   public getPhotos(): Observable<Photo[]>{
     return this.http.get<Photo[]>('http://localhost:3000/photos')
+      .pipe(map(photos => {
+        return photos.map(photo => {
+          return {...photo, description: photo.description.toUpperCase()}
+        })
+      }))
       .pipe(delay(2000))
   }
 
