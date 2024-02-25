117ef797220bb18fd53b5de74ec96ed6c0fae127
diegocantelli <diegocantelli19@gmail.com>
2024-02-25 12:00:16 -0300

    Injetando um servico no teste atraves de TestBed.inject

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -2,11 +2,13 @@ import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { PhotoListComponent } from './photo-list.component';
 import { PhotoListModule } from './photo-list.module';
 import { HttpClientModule } from '@angular/common/http';
+import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
 
 describe
 (PhotoListComponent.name, () => {
   let fixture: ComponentFixture<PhotoListComponent>
   let component: PhotoListComponent
+  let service: PhotoBoardService
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
@@ -15,9 +17,14 @@ describe
 
     fixture = TestBed.createComponent(PhotoListComponent)
     component = fixture.componentInstance
+    service = TestBed.inject(PhotoBoardService)
   })
 
   it('Should create component', () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('Should display board when data arrives', () => {
     expect(component).toBeTruthy()
   })
 })
